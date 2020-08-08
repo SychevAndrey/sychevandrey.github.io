1bcf17b1f2cd2f250a687ba167e33340499bdf8e
Andrey S <nukemoore@gmail.com>
2020-08-08 18:14:10 +0300

    add kitty drum kit

---
+++ kitty-drum-kit/script.js
@@ -0,0 +1,30 @@
+const keys = {};
+const sounds = {};
+
+document.querySelectorAll('.key').forEach((keyElement) => {
+  keys[keyElement.dataset.key] = keyElement;
+  keyElement.addEventListener('transitionend', (e) => {
+    if (e.propertyName !== 'transform') return;
+    keyElement.classList.remove('key--playing');
+  })
+});
+
+document.querySelectorAll('audio').forEach((audioElement) => {
+	sounds[audioElement.dataset.key] = audioElement;
+});
+
+window.addEventListener('keydown', (e) => {
+  if (keys.hasOwnProperty(e.code)) {
+    keys[e.code].classList.add('key--playing');
+  }
+  if (sounds.hasOwnProperty(e.code)) {
+    sounds[e.code].currentTime = 0;
+    sounds[e.code].play();
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  if (keys.hasOwnProperty(e.code)) {
+    setTimeout(() => {keys[e.code].classList.remove('key--playing')}, 200);
+  }
+});
